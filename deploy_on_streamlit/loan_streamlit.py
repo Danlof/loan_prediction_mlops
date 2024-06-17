ba9445b39562a6156cdb8b9ac888e616a0ff2a2a
danlof <danmusyoki.dk@gmail.com>
2024-06-17 06:48:11 +0300

    wget

--- deploy_on_streamlit/loan_streamlit.py
+++ deploy_on_streamlit/loan_streamlit.py
@@ -6,7 +6,7 @@ import wget
 
 # load model
 model_name ='RF_Loan_model.joblib'
-file_url="https://github.com/Danlof/loan_prediction_mlops/blob/db04652af5d4b5058be3351c7b99444e6102ae18/deploy_on_streamlit/RF_Loan_model.joblib"
+file_url="https://github.com/Danlof/loan_prediction_mlops/blob/main/deploy_on_streamlit/RF_Loan_model.joblib"
 wget.download(file_url)
 model = joblib.load(model_name)
 
